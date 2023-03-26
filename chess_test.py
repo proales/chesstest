23eff03983cc5bac023cbf98d9349ec72f9d718f
Paul Roales <proales@proales.com>
2023-03-25 19:46:40 -0700

    initial code for testing chess pieces
    .

---
+++ chess_test.py
@@ -0,0 +1,34 @@
+def is_legal_move(piece, start, end):
+    # Define the row and column differences between the start and end positions.
+    row_diff = abs(end[0] - start[0])
+    col_diff = abs(end[1] - start[1])
+    
+    # Define the legal moves for each type of chess piece.
+    if piece == 'king':
+        # The king can move one step in any direction.
+        return row_diff <= 1 and col_diff <= 1
+    elif piece == 'queen':
+        # The queen can move any number of steps in any direction.
+        return row_diff == col_diff or start[0] == end[0] or start[1] == end[1]
+    elif piece == 'rook':
+        # The rook can move any number of steps horizontally or vertically.
+        return start[0] == end[0] or start[1] == end[1]
+    elif piece == 'bishop':
+        # The bishop can move any number of steps diagonally.
+        return row_diff == col_diff
+    elif piece == 'knight':
+        # The knight can move in an L-shape (2 steps in one direction and 1 step in a perpendicular direction).
+        return (row_diff == 1 and col_diff == 2) or (row_diff == 2 and col_diff == 1)
+    elif piece == 'pawn':
+        # The pawn can move one step forward (assuming forward is in the positive row direction).
+        # For simplicity, capturing and initial two-step moves are not considered.
+        return row_diff == 1 and col_diff == 0
+    else:
+        # If an invalid piece type is provided, return False.
+        return False
+
+# Example usage
+piece_type = 'knight'
+start_position = (3, 3)  # Row 3, Column 3
+end_position = (5, 4)    # Row 5, Column 4
+print(is_legal_move(piece_type, start_position, end_position))
